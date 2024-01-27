e02a91f7dd0f9025f11225d8766a2a9998f26813
seetha32 <lingam.seetha32@gmail.com>
2024-01-27 18:29:44 +0000

    Tasty kichen

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+
+import AllRestaurantSection from '../AllRestaurantSection'
+import Footer from '../Footer'
+
+const Home = () => (
+  <>
+    <Header />
+    <AllRestaurantSection />
+    <Footer />
+  </>
+)
+
+export default Home
